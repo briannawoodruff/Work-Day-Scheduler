15f5e5498aba856675d76cb346e73a1fa7106288
Brianna Woodruff <briannaewoodruff@gmail.com>
2021-10-28 21:51:16 -0500

    added time tracker

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Work-Day-Scheduler
+05 Third-Party APIs: Work Day Scheduler
+
+A simple calender to keep track of a work day schedule. Each item inputed is stored in local storage. This application utilizes MomentJS to display the current date and keeps track of the time to see if it is current, past, or present to when viewing the schedule.
+
+https://briannawoodruff.github.io/Work-Day-Scheduler/
+
+<img src="assets\images\ScreenCapture-WorkDayScheduler.jpg" title="Work Day Scheduler">
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,14 @@
-// reference to important DOM elements
+// reference to DOM elements
 var timeDisplayEl = $('#currentDay');
 var saveBtn = $('.saveBtn');
-// var description = $('.description');
-var timeBlock = $('.time-block');
-var hour = $('.hour');
 
+// WHEN I refresh the page
+// THEN the saved events persist
+// set up local storage and first thing that is read
 var calenderInput = localStorage.getItem("dataObject") || "[]";
 var calenderInputArr = JSON.parse(calenderInput);
 
-
-// loop through each item in the array and grab
+// loop through each item in the array, grab each item to then display if there is an item
 for (i = 0; i < calenderInputArr.length; i++) {
   var timeID = calenderInputArr[i].time
   var valueVal = calenderInputArr[i].value
@@ -23,43 +22,54 @@ for (i = 0; i < calenderInputArr.length; i++) {
 // THEN the current day is displayed at the top of the calendar
 // handle displaying the time
 function displayTime() {
-    var rightNow = moment();
-    timeDisplayEl.text(rightNow.format('MMM DD, YYYY'));
-  }
+  var rightNow = moment();
+  timeDisplayEl.text(rightNow.format('MMM DD, YYYY'));
+}
 displayTime();
 
 
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 /* 1. create a function that uses momentjs to know what the current time is and then show on the calender if it's in the past, present, or future with the classes in css
-for loop [$("").each(function ()] and if/ else statements in jquery? */ 
-// handle project form submission
-function scheduleTime() {
-    var currentTime = moment().hour();
+for loop [$("").each(function ()] and if/ else statements in jquery? */
+function timeTracker() {
+  var currentTime = moment().hour();
 
-    // loop over each time block to see what time it is
-    $(timeBlock).each(function() {
-
-    })
-  
-  }
-  
-
+  // loop over each time block to compare to current time
+  $(".time-block").each(function () {
+    // parsing through each time block to get an integer to compare to the time integer
+    var blockTime = parseInt($(this).siblings(".description").attr("id"));
+    
+    // past time
+    if (blockTime < currentTime) {
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+    }
+    // present time
+    else if (blockTime === currentTime) {
+      $(this).removeClass("future");
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    }
+    // future time
+    else {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    }
+  })
+}
 
 // WHEN I click the save button for that timeblock
 // THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-/* 1. setting local storage in jquery for each time?
-2. creating an .on click event to do that
-*/
-// $(description).val(localStorage.getItem(description, hour))
-
-saveBtn.on("click", function() {
+// add .on to the save button and save the time and description that is input
+saveBtn.on("click", function () {
   var time = $(this).siblings(".description").attr("id")
   var text = $(this).siblings(".description").val()
-  calenderInputArr.push({time:time, value:text})
+  calenderInputArr.push({ time: time, value: text })
   console.log(calenderInputArr)
   localStorage.setItem("dataObject", JSON.stringify(calenderInputArr))
 })
-
+// run again
+timeTracker();
